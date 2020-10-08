3bec94e94971747c6ff2f91556141d4109ef955c
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-08 20:53:23 +0100

    Add styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+function Task({ task, id }) {
+  console.log(task);
+  return <div className="task">{task.text}</div>;
+}
+
 function App() {
-  return <div className="App"></div>;
+  const [tasks, setTasks] = useState([
+    { id: 1, text: 'I am text', completed: false },
+    { id: 2, text: 'I am task', completed: false },
+    { id: 3, text: 'I am groot', completed: false },
+  ]);
+
+  return (
+    <div className="App">
+      <div className="task-list">
+        {tasks.map((task) => (
+          <Task key={task.id} task={task} id={task.id}></Task>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
